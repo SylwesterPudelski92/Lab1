427cd167644f6599759e448f4b22c31ea00baf3e
SylwesterPudelski92 <sylwesterpudelski1992@gmail.com>
2022-04-19 08:46:32 +0200

    LAB1

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,7 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Integer;
+
+
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Hello java world");
-    }
-}
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.setName("sylwester Pudelski");
+        System.out.println(acc.getName());
+}}
+
